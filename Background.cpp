42518513ce370a89624ad505a18d402ed3246f41
tacl_na <lechianhtuan1903@gmail.com>
2025-02-01 13:49:37 +0700

    first commit

---
+++ Background.cpp
@@ -0,0 +1,58 @@
+﻿#include "Background.h"
+#include <cmath>
+#include <iostream>
+
+Background::Background(SDL_Renderer* renderer, const std::string& imagePath, SDL_Rect screen)
+    : screenSize(screen), transitionRadius(0), transitioning(false), toBlack(false), currentState(Penguin::CANDLE_OFF) {
+    // Load texture của ảnh nền
+    SDL_Surface* imageSurface = IMG_Load(imagePath.c_str());
+    if (!imageSurface) {
+        std::cerr << "Unable to load image: " << imagePath << " " << IMG_GetError() << std::endl;
+        imageTexture = nullptr;
+    }
+    else {
+        imageTexture = SDL_CreateTextureFromSurface(renderer, imageSurface);
+        SDL_FreeSurface(imageSurface);
+    }
+}
+
+Background::~Background() {
+    if (imageTexture) SDL_DestroyTexture(imageTexture);
+}
+
+void Background::update(Penguin::State newState) {
+    if (newState != currentState) {
+        currentState = newState;
+    }
+}
+
+void Background::render(SDL_Renderer* renderer) {
+    if (currentState == Penguin::CANDLE_OFF) {
+        // Hiển thị nền đen
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_RenderClear(renderer);
+    }
+    else {
+        // Hiển thị ảnh nền
+        if (imageTexture) {
+            SDL_RenderCopy(renderer, imageTexture, nullptr, nullptr);
+        }
+    }
+}
+
+SDL_Texture* Background::loadTexture(const std::string& path, SDL_Renderer* renderer) {
+    SDL_Surface* loadedSurface = IMG_Load(path.c_str());
+    if (!loadedSurface) {
+        std::cerr << "Unable to load image " << path << ": " << IMG_GetError() << std::endl;
+        return nullptr;
+    }
+
+    SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, loadedSurface);
+    SDL_FreeSurface(loadedSurface);
+
+    if (!texture) {
+        std::cerr << "Unable to create texture from " << path << ": " << SDL_GetError() << std::endl;
+    }
+
+    return texture;
+}
